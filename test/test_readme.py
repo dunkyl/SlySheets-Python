25746a45304eb0a16226a6190cb4c3c799c8adfd
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2022-02-24 02:38:20 -0800

    update readme

--- test/test_readme.py
+++ test/test_readme.py
@@ -12,29 +12,29 @@ async def test_readme():
 
     # A1 notation
     a1 = await sheet['A1']
-    print(F"Cell A1: {a1}")
+    print(F"Cell A1: {a1}") # Cell A1: Foo
 
     # or zero-indexed row/col
     first_cell = await sheet[0, 0]
-    print(F"Cell A1 (again): {first_cell}")
+    print(F"Cell A1 (again): {first_cell}") # Cell A1 (again): Foo
 
     # zero-indexed rows
     first_row = await sheet[0]
     assert isinstance(first_row, ResultRow)
-    print(F" | {first_row[0]:8} | {first_row[1]:8} |")
+    print(F" | {first_row[0]:6} | {first_row[1]:6} |") # | Foo     | Bar     |
 
     # header-indexed columns
     foos = await sheet['Foo'] # list[Any]
-    print(F"Foos: {foos}")
+    print(F"Foos: {foos}") # Foos: [1, 2, 3, 26]
 
     # slicing and iterators
     # TODO: async iters like ytdapi
     for row in await sheet[1:3]:
         # index result by header
-        print(F" | {row['Foo']:8} | {row['Bar']:8} |")
+        print(F" | {row['Foo']:6} | {row['Bar']:6} |") # |      1 | a     | etc...
     for row in await sheet[3:]:
         # or by column
-        print(F" | {row['A']:8} | {row['B']:8} |")
+        print(F" | {row['A']:6} | {row['B']:6} |") # |    26 | z     | etc...
 
     # append, of course
     await sheet.append([0, 'x'])
